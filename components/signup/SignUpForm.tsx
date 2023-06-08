e4efe3ba15068eb28ea863eee7f9f7be469aa3ba
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-06-08 10:39:51 +0800

    Refactoring

--- components/signup/SignUpForm.tsx
+++ components/signup/SignUpForm.tsx
@@ -9,43 +9,41 @@ type Props = {
 const SignUpForm = ({ steps }: Props) => {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const page = searchParams.get("page");
+  const page = searchParams.get("step");
   const pageIndex = page ? +page : 1;
   const Step = steps.at(pageIndex - 1)!;
   return (
-    <div>
-      <Formik
-        onSubmit={(values: any) => {
-          console.log(values);
-        }}
-        initialValues={{
-          email: "",
-          password: "",
-          firstName: "",
-          lastName: "",
-          interests: "",
-        }}
-      >
-        <Form>
-          <Step
-            onNext={() => {
-              if (pageIndex < steps.length) {
-                const nextPage = pageIndex + 1;
-                router.push(`/signup?page=${nextPage}`);
-              }
-            }}
-            onPrevious={() => {
-              const prevPage = pageIndex - 1;
-              if (prevPage > 1) {
-                router.push(`/signup?page=${prevPage}`);
-              } else {
-                router.push("/signup");
-              }
-            }}
-          />
-        </Form>
-      </Formik>
-    </div>
+    <Formik
+      onSubmit={(values: any) => {
+        console.log(values);
+      }}
+      initialValues={{
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        interests: "",
+      }}
+    >
+      <Form>
+        <Step
+          onNext={() => {
+            if (pageIndex < steps.length) {
+              const nextPage = pageIndex + 1;
+              router.push(`/signup?step=${nextPage}`);
+            }
+          }}
+          onPrevious={() => {
+            const prevPage = pageIndex - 1;
+            if (prevPage > 1) {
+              router.push(`/signup?step=${prevPage}`);
+            } else {
+              router.push("/signup");
+            }
+          }}
+        />
+      </Form>
+    </Formik>
   );
 };
 
